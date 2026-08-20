de88d64993c32936da577c1cd0d8d65ec8ca2a65
simonedeitos <info@simonedeitos.it>
2026-08-20 18:40:54 +0200

    Delete cron directory

--- cron/cleanup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/../app/bootstrap.php';
-
-$anonymized = app('analytics')->anonymizeOldClicks();
-app('cache')->appendJsonLine('logs', 'cron.log', ['job' => 'cleanup', 'anonymized' => $anonymized, 'created_at' => date('c')]);
-echo "Anonymized: {$anonymized}" . PHP_EOL;
